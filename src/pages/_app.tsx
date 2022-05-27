ff48981b0a1e2cb3f609b66e9cb5ffc436831942
yoneyama ryo <uryonym@gmail.com>
2022-05-27 20:41:44 +0900

    プロジェクト全体をCSR化

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import dynamic from 'next/dynamic'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const SafeHydrate = dynamic(() => import('@/components/SafeHydrate'), { ssr: false })
+
+  return (
+    <SafeHydrate>
+      <Component {...pageProps} />
+    </SafeHydrate>
+  )
 }
 
 export default MyApp
