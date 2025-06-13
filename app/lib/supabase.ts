c5571c77673a3ae6e4238044bdad542e2ed3ce46
ryo yoneyama <uryonym@gmail.com>
2025-06-14 06:52:34 +0900

    tmp commit

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,3 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
+import { type Database } from '~/types/database.types'
 
-export const supabase = createClient(import.meta.env.VITE_SUPABASE_API_URL!, import.meta.env.VITE_SUPABASE_API_KEY!)
+export const supabase = createClient<Database>(
+  import.meta.env.VITE_SUPABASE_API_URL!,
+  import.meta.env.VITE_SUPABASE_API_KEY!,
+)
--- app/routes/task.tsx
+++ app/routes/task.tsx
@@ -1,18 +1,51 @@
 import { supabase } from '~/lib/supabase'
-import { redirect } from 'react-router'
+import type { Route } from './+types/task'
 
 export async function clientLoader() {
-  const userData = (await supabase.auth.getUser()).data.user
-  console.log(userData)
-  if (!userData) {
-    return redirect('/login')
-  }
+  const { data: tasks, error } = await supabase.from('tasks').select()
+  return { tasks, error }
 }
 
-export default function Task() {
+export default function Task({ loaderData }: Route.ComponentProps) {
+  const data = loaderData.tasks
+
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h2 className="text-4xl font-bold">task screen</h2>
+    <div className="flex-1 flex flex-col gap-3 p-2">
+      <div>
+        <button className="underline" type="button" onClick={}>
+          新規作成
+        </button>
+      </div>
+      <ul>
+        {incompleteTasks &&
+          incompleteTasks.map((task) => (
+            <li className="flex gap-3 items-center p-2 border-b-2" key={task.id} onClick={() => handleClickEdit(task)}>
+              <div>
+                <input type="checkbox" defaultChecked={task.isComplete} />
+              </div>
+              <div>
+                <p>{task.title}</p>
+                <p className="text-sm">{task.deadLine?.toLocaleDateString('ja-JP')}</p>
+              </div>
+            </li>
+          ))}
+      </ul>
+      <p>完了済タスク</p>
+      <ul>
+        {completeTasks &&
+          completeTasks.map((task) => (
+            <li className="flex gap-3 items-center p-2 border-b-2" key={task.id} onClick={() => handleClickEdit(task)}>
+              <div>
+                <input type="checkbox" defaultChecked={task.isComplete} />
+              </div>
+              <div>
+                <p>{task.title}</p>
+                <p className="text-sm">{task.deadLine?.toLocaleDateString('ja-JP')}</p>
+              </div>
+            </li>
+          ))}
+      </ul>
+      <TaskModal show={openModal} onClose={handleCloseModal} task={currentTask} />
     </div>
   )
 }
