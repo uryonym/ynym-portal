c37931f9c10242afa01878e00a21f13aadda2f00
yoneyama ryo <uryonym@gmail.com>
2025-06-24 01:37:44 +0900

    layout.tsxにdynamicエクスポートを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import AppSidebar from '@/components/AppSidebar'
 import Header from '@/components/Header'
 import { SidebarProvider } from '@/components/ui/sidebar'
 
+export const dynamic = 'force-dynamic'
+
 export const metadata: Metadata = {
   title: 'ynym-portal',
   description: '米山家のポータルサイト',
