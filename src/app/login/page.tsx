708b5c4ab99b3200ae0de13edec78931e9fcbf6e
yoneyama ryo <uryonym@gmail.com>
2025-07-09 16:13:13 +0900

    ログインコンポーネントを簡素化し、セッションに基づく表示を削除

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,29 +1,14 @@
-import { auth } from '@/auth'
 import { Button } from '@/components/ui/button'
 
-import { login, logout } from '../actions/login'
+import { login } from '../actions/login'
 
-const Login = async () => {
-  const session = await auth()
-
+const Login = () => {
   return (
-    <>
-      {session !== null ? (
-        <div className="flex h-screen flex-col items-center justify-center gap-4">
-          <p>{session.user?.name}がログインしています</p>
-          <img src={session.user?.image} alt="user image" />
-          <form action={logout}>
-            <Button type="submit">ログアウト</Button>
-          </form>
-        </div>
-      ) : (
-        <div className="flex h-screen flex-col items-center justify-center gap-4">
-          <form action={login}>
-            <Button type="submit">ログイン</Button>
-          </form>
-        </div>
-      )}
-    </>
+    <div className="flex h-screen flex-col items-center justify-center gap-4">
+      <form action={login}>
+        <Button type="submit">ログイン</Button>
+      </form>
+    </div>
   )
 }
 
