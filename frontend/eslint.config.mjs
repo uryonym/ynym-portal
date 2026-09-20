7788b19420d563834d1e25a076a7b3e89baf2679
yoneyama ryo <uryonym@gmail.com>
2026-09-20 16:05:48 +0900

    refactor: eslint-plugin-react-refreshの削除と設定整理

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,7 +1,6 @@
 import js from '@eslint/js'
 import prettier from 'eslint-config-prettier'
 import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
@@ -25,14 +24,9 @@ export default tseslint.config(
     },
     plugins: {
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
       '@typescript-eslint/no-unused-vars': [
         'warn',
         {
@@ -40,12 +34,6 @@ export default tseslint.config(
           varsIgnorePattern: '^_',
         },
       ],
-    },
-  },
-  {
-    files: ['src/routes/**/*.{ts,tsx}', 'src/components/ui/**/*.{ts,tsx}'],
-    rules: {
-      'react-refresh/only-export-components': 'off',
     },
   },
   prettier,
