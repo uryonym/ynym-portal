db15412e92aa2253b1e5fa31a131cd45e7c4c316
yoneyama ryo <uryonym@gmail.com>
2026-09-20 23:58:10 +0900

    refactor: ESLintのimport/orderルールを設定

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -22,8 +22,39 @@ export default [
     rules: {
       // 公式テンプレート推奨の off 設定
       'import/no-cycle': 'off',
-      'import/order': 'off',
       'sort-imports': 'off',
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            ['parent', 'sibling'],
+            'index',
+            'object',
+            'type',
+          ],
+          pathGroups: [
+            {
+              pattern: '{react,react-dom}',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: '@/**',
+              group: 'internal',
+              position: 'before',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['builtin', 'type'],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
       '@typescript-eslint/array-type': 'off',
       '@typescript-eslint/require-await': 'off',
       'pnpm/json-enforce-catalog': 'off',
