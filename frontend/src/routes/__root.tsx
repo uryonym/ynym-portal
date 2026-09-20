95fcf77870a07b99240e8948a9e158dd69cfec41
yoneyama ryo <uryonym@gmail.com>
2026-09-20 16:09:52 +0900

    refactor: react-router-devtoolsへの移行

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,5 +1,5 @@
 import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import type { QueryClient } from '@tanstack/react-query'
 import { Toaster } from '@/components/ui/sonner'
